eb794c54ba8ecf0a54a861c4d3156a18e6353d96
ankur362 <ankurdynamic786@gmail.com>
2025-02-10 13:57:55 +0530

    added all  the function of calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,75 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.Stack;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static int rule(char ops) {
+        if (ops == '+' || ops == '-') return 1;
+        else if (ops == '*' || ops == '/') return 2;
+        return 0;
+    }
+
+    public static int calculationrule(int b, int a, char op) {
+        switch (op) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException("Number cannot be divided by 0");
+                }
+                return a / b;
+            default: return 0;
+        }
+    }
+
+    public static int calculation(String exp) {
+        Stack<Integer> ans = new Stack<>();
+        Stack<Character> operation = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (c == '(') {
+                operation.push(c);
+            } else if (c == ' ') {
+                continue;
+            } else if (c == ')') {
+                while (!operation.isEmpty() && operation.peek() != '(') {
+                    ans.push(calculationrule(ans.pop(), ans.pop(), operation.pop()));
+                }
+                operation.pop();
+            } else if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < exp.length() && Character.isDigit(exp.charAt(i))) {
+                    num = num * 10 + (exp.charAt(i) - '0');
+                    i++;
+                }
+                i--;
+                ans.push(num);
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                while (!operation.isEmpty() && rule(c) <= rule(operation.peek())) {
+                    ans.push(calculationrule(ans.pop(), ans.pop(), operation.pop()));
+                }
+                operation   .push(c);
+            }
+        }
+
+        while (!operation.isEmpty()) {
+            ans.push(calculationrule(ans.pop(), ans.pop(), operation.pop()));
+        }
+
+        return ans.pop();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String exp = sc.nextLine();
+        try {
+            System.out.println(calculation(exp));
+        } catch (Exception e) {
+            System.out.println("invalid Argument " );
         }
     }
 }
